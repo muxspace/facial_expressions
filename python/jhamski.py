3620f8df086d64687591d06191ad0a9229b4213e
Jim <hamski.2@gmail.com>
2016-11-05 15:24:52 -0400

    Save image, two warps added. Need 8 more transformations.

--- python/jhamski.py
+++ python/jhamski.py
@@ -2,11 +2,10 @@
 
 import csv
 import os
+import math
 
 from skimage import io
-
-
-#import pandas as pd
+from skimage import transform as tf
 
 # Import legend.csv and save my (jhamski) image files to a list
 script_dir = os.path.dirname(__file__)
@@ -15,9 +14,8 @@ full_path_csv = os.path.join(script_dir, '../data/legend.csv')
 ifile  = open(full_path_csv, "rb")
 reader = csv.reader(ifile)
 
-rownum = 0
+#rownum = 0
 jhamski_pics = []
-
 for row in reader:
     if row[0] == 'jhamski':
         pic_name = row[1]
@@ -25,18 +23,40 @@ for row in reader:
 
 ifile.close()
 
-#read in images
+#function to read in images
 
 def read_image(image_name, script_dir):
 
-    image_path = os.path.join(script_dir, '../data/%s' % image_name)
+    image_path = os.path.join(script_dir, '../images/%s' % image_name)
     img = io.imread(image_path)
     return img
+
+def save_image(img, name, trans_type):
+    name = os.path.splitext(name)[0]
+    io.imsave(name + "_" + trans_type + '.png', img,)
 
 
 #setup image transformation functions
 
-# read in each image in the in the list, transform, and save
+def warp1(img, name):
+    tform = tf.SimilarityTransform(scale=1, rotation=math.pi / 4,
+                               translation=(img.shape[0] / 2, -100))
+    af_img = tf.warp(img, tform)
 
-for i in range(0, len(jhamski_pics)):
-    read_image(jhamski_pics[i])
+    save_image(af_img, name, 'warp1')
+
+def warp2(img, name):
+    tform = tf.SimilarityTransform(scale=1, rotation=math.pi / -4,
+                               translation=(img.shape[0] / 5, 100))
+    af_img = tf.warp(img, tform)
+
+    save_image(af_img, name, 'warp2')
+
+
+# read in each image in the in the list, transform, and save
+# swap out testing length 5 for len(jhamski_pics)
+for i in range(0, 5):
+    img = read_image(jhamski_pics[i], script_dir)
+
+    warp1(img, jhamski_pics[i])
+    warp2(img, jhamski_pics[i])
