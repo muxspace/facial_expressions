acec48d6d45b32d203ed4d710b0f3500ff8ccf1d
SDSPS <dundeva@yahoo.com>
2016-11-06 23:21:44 -0500

    Create 457.py
    Image Augmentation Function

---
+++ python/457.py
@@ -0,0 +1,241 @@
+ coding: utf-8
+
+# In[ ]:
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+import theano
+from PIL import Image
+from numpy import *
+
+
+
+# input image dimensions
+img_rows, img_cols = 32,32
+
+# number of channels
+img_channels =1
+
+
+#  data
+
+path1 = 'C:/Users/dundeva/Documents/CroppedImages'    #path of folder of images    
+path2 = 'C:/Users/dundeva/Documents/AugmentedImages'  #path of folder to save images    
+
+listing = os.listdir(path1) 
+num_samples=size(listing)
+print num_samples
+
+
+for file in listing:
+    im = Image.open(path1 + '//' + file)   
+    img = im.resize((img_rows,img_cols))
+    gray = img.convert('L')
+                #need to do some more processing here           
+    gray.save(path2 +'//' +  file, "JPEG")
+
+imlist = os.listdir(path1)
+
+im1 = array(Image.open('C:/Users/dundeva/Documents/AugmentedImages' + '//'+ imlist[0])) # open one image to get size
+m,n = im1.shape[0:2] # get the size of the images
+imnbr = len(imlist) # get the number of images
+
+# create matrix to store all flattened images
+immatrix = array([array(Image.open('C:/Users/dundeva/Documents/AugmentedImages'+ '//' + im2)).flatten()
+              for im2 in imlist],'f')
+                
+label=np.ones((num_samples,),dtype = int)
+label[0:89]=0
+label[89:187]=1
+label[187:]=2
+
+
+data,Label = shuffle(immatrix,label, random_state=2)
+train_data = [data,Label]
+
+img=immatrix[167].reshape(img_rows,img_cols)
+plt.imshow(img)
+plt.imshow(img,cmap='gray')
+print (train_data[0].shape)
+print (train_data[1].shape)
+
+
+
+#batch_size to train
+batch_size = 32
+# number of output classes
+nb_classes =10
+# number of epochs to train
+nb_epoch =200
+data_augmentation = True
+
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 3
+# convolution kernel size
+nb_conv = 3
+
+(X, y) = (train_data[0],train_data[1])
+
+
+# split X and y into training and testing sets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+
+X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+i = 100
+plt.imshow(X_train[i, 0], interpolation='nearest')
+print("label : ", Y_train[i,:])
+
+
+model = Sequential()
+model.add(Convolution2D(32, 4, 4, border_mode='same',
+                        input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 4, 4))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+
+
+hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
+            
+            
+hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              show_accuracy=True, verbose=1, validation_split=0.2)
+
+
+# visualizing losses and accuracy
+
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(nb_epoch)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              validation_data=(X_test, Y_test),
+              shuffle=True)
+else:
+    print('Using real-time data augmentation.')
+
+    # this will do preprocessing and realtime data augmentation
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied)
+    datagen.fit(X_train)
+
+    # fit the model on the batches generated by datagen.flow()
+    model.fit_generator(datagen.flow(X_train, Y_train,
+                        batch_size=batch_size),
+                        samples_per_epoch=X_train.shape[0],
+                        nb_epoch=nb_epoch,
+                        validation_data=(X_test, Y_test))
+
+
+#%%       
+
+score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+print(model.predict_classes(X_test[1:5]))
+print(Y_test[1:5])
+
+
+# In[ ]:
+
